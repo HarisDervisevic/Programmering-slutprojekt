2161c414b7d17093b51989aa32b38d3d550db008
harisssssss <54796022+HarisDervisevic@users.noreply.github.com>
2021-05-18 12:19:49 +0200

    simple movement

--- main/main/IMovement.cs
+++ main/main/IMovement.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace main
 {
-	class ballMovement
+	class IMovement
 	{
+        
 	}
 }
--- main/main/MainWindow.xaml.cs
+++ main/main/MainWindow.xaml.cs
@@ -13,16 +13,106 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace main
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+
+    public partial class MainWindow : Window
+    {
+
+        bool goUp;
+        bool goDown;
+        int playerSpeed = 7;
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            myCanvas.Focus();
+        }
+
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+            {
+                if (Canvas.GetTop(player1) > 0.0)
+                {
+                    Canvas.SetTop(player1, Canvas.GetTop(player1) - playerSpeed);
+                }
+
+            }
+            if (e.Key == Key.W)
+            {
+                if (Canvas.GetTop(player2) > 0.0)
+                {
+
+                    Canvas.SetTop(player2, Canvas.GetTop(player2) - playerSpeed);
+                }
+
+            }
+
+            if (e.Key == Key.Down)
+            {
+                if (Canvas.GetTop(player1) + player1.Height < myCanvas.ActualHeight)
+                {
+                    Canvas.SetTop(player1, Canvas.GetTop(player1) + playerSpeed);
+                }
+            }
+
+            if (e.Key == Key.S)
+            {
+                if (Canvas.GetTop(player2) + player2.Height < myCanvas.ActualHeight)
+                {
+                    Canvas.SetTop(player2, Canvas.GetTop(player2) + playerSpeed);
+                }
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+            switch (e.Key)
+            {
+                case Key.W:
+                    {
+                        goUp = true;
+                        goDown = false;
+                        break;
+                    }
+
+                case Key.S:
+                    {
+                        goUp = false;
+                        goDown = true;
+                        break;
+                    }
+
+                case Key.Up:
+                    {
+                        goUp = true;
+                        goDown = false;
+                        break;
+                    }
+
+                case Key.Down:
+                    {
+                        goUp = false;
+                        goDown = true;
+                        break;
+                    }
+
+
+
+            }
+
+
+        }
+    }
+
 }
